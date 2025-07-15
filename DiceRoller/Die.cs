6ac1da6ebd1af47c774afa8b09ada86e58c70eb5
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-07-15 12:37:18 -0700

    Created a Die class

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,19 @@
+﻿namespace DiceRoller;
+
+/// <summary>
+/// Represents a customizable die.
+/// </summary>
+/// <remarks>This class can be used to simulate a custom die in games or simulations. The number of sides can be set to
+/// any positive byte value (1-255 inclusive).</remarks>
+public class Die
+{
+    public Die(byte numSides)
+    {
+        if (numSides < 1 || numSides > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numSides), "The number of sides must be between 1 and 20.");
+        }
+        this.numSides = numSides;
+    }
+    public byte numSides { get; private set; }
+}

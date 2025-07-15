c171bf8a6b5f5e533a5cfd572f4d6020cb4d560f
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-07-15 13:18:58 -0700

    Update Die class for improved functionality and clarity
    - Adjusted documentation to limit sides to 1-20.
    - Changed `rand` to a static variable for shared use.
    - Added optional `auto_roll` parameter in the constructor.
    - Renamed `numSides` to `NumberOfSides` for clarity.
    - Updated `Roll` method to use `NumberOfSides`.

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -4,13 +4,13 @@
 /// Represents a customizable die.
 /// </summary>
 /// <remarks>This class can be used to simulate a custom die in games or simulations. The number of sides can be set to
-/// any positive byte value (1-255 inclusive).</remarks>
+/// any positive byte value (1-20 inclusive).</remarks>
 public class Die
 {
     private const byte MAX_SIZE = 20;
 
     // Private Variables
-    private readonly Random rand = new();
+    private static readonly Random rand = new();
 
 
     /// <summary>
@@ -22,15 +22,20 @@ public class Die
     /// <exception cref="ArgumentOutOfRangeException">
     /// Thrown when <paramref name="numSides"/> is less than 1 or greater than 20.
     /// </exception>
-    public Die(byte numSides)
+    public Die(byte numSides = 6, Boolean auto_roll = false)
     {
         if (numSides < 1 || numSides > MAX_SIZE)
         {
             throw new ArgumentOutOfRangeException(nameof(numSides), "The number of sides must be between 1 and 20.");
         }
-        this.numSides = numSides;
+        this.NumberOfSides = numSides;
+
+        if (auto_roll)
+        {
+            Roll();
+        }
     }
-    public byte numSides { get; private set; }
+    public byte NumberOfSides { get; private set; }
 
     /// <summary>
     /// The current face-up value of the die.
@@ -44,9 +49,9 @@ public class Die
     /// <returns>The face-up value of the die after rolling.</returns>
     public byte Roll()
     {
-        // Generate a random number between 1 and numSides (inclusive)
+        // Generate a random number between 1 and NumberOfSides (inclusive)
         // Note: (byte) is like Convert.ToByte() but more efficient
-        FaceUpValue = (byte)rand.Next(1, numSides + 1);
+        FaceUpValue = (byte)rand.Next(1, NumberOfSides + 1);
 
         return (byte)FaceUpValue;
     }
